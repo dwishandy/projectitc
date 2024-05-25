fcec68f0722fe3e2f233b67d9173f3930a0f3ffa
firshandy <firshandydwi@gmail.com>
2024-05-25 08:05:13 +0700

    add contact us page

---
+++ contactus/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        homebg: "url('assets/bghome.png')",
+      },
+      fontFamily: {
+        poppins: ["Poppins"],
+        montserrat: ["Montserrat"],
+      },
+      colors: {
+        mainblue: "#3D85D7",
+        bluetxt: "#0B417D",
+        blueline: "#0A4587",
+        footer: "#0A4587",
+      },
+      screens: {
+        "3xl": "1440px",
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar")],
+};
