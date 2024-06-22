e6078c384edacf8338031bb89dc1df040630d23f
firshandy <firshandydwi@gmail.com>
2024-06-22 14:31:09 +0700

    add .gitignore to all folder and add structure page

---
+++ structure/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+        montserrat: ["Montserrat"],
+      },
+      colors: {
+        mainblue: "#3D85D7",
+        bluebg: "#0B417D",
+        blueline: "#0A4587",
+        footer: "#0A4587",
+        bluebgperson: "#3D85D7",
+      },
+      screens: {
+        "3xl": "1440px",
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar")],
+};
