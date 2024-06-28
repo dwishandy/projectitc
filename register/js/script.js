b73d6ee8a4911b98777cba370d0ad5ecb38e8f1d
firshandy <firshandydwi@gmail.com>
2024-06-28 21:30:45 +0700

    revisi

--- register/js/script.js
+++ register/js/script.js
@@ -5,15 +5,15 @@ function sendMessage() {
   let divisi = document.getElementById("divisi").value;
 
   let url =
-    "https://api.whatsapp.com/send?phone=6285691278619&text=Halo%20Kak%0A%0ASaya%20*" +
+    "https://api.whatsapp.com/send?phone=6285691278619&text=Halo%20Kak%0A%0ASaya%20" +
     name +
-    "*%0AKelas%20*" +
+    "%0AKelas%20" +
     kelas +
-    "*%0AJurusan%20*" +
+    "%0AJurusan%20" +
     jurusan +
-    "*%0A%0AIngin%20mendaftar%20IT%20Club%20di%20divisi%20*" +
+    "%0A%0AIngin%20mendaftar%20IT%20Club%20di%20divisi%20" +
     divisi +
-    "*%0A%0ATerimakasih";
+    "%0A%0ATerimakasih";
 
   window.open(url).focus();
 }
